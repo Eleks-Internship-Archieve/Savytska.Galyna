981cf7f8ad78f1e348c72a1f63d3d4da68b97a6b
Savytska <otakadivchynka@gmail.com>
2016-06-14 11:02:03 +0300

    Second

--- location.js
+++ location.js
@@ -1,10 +1,11 @@
 var map;
-
+var service;
 function initMap(location) {
     
-    console.log(location);
+    console.log(location);  
     
     var currentLocation = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
+
     var mapOptions =  {
         center: currentLocation,
         zoom: 15
@@ -12,10 +13,30 @@ function initMap(location) {
     
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     
-    var marker = new google.maps.Marker({
-        position: currentLocation,
-        map: map
-    });
+    var markerLoc = new google.maps.Marker({
+    map: map,
+    position: currentLocation
+  });
+    
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: currentLocation,
+        radius: 10000, 
+        types: ['school']
+    }, callback);
 }
+
+function callback(results, status) {    
+    console.log(results);
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var marker = new  google.maps.Marker({
+    map: map,
+    position: results[i].geometry.location
+  });
+    }
+  }
+}
+
 
 navigator.geolocation.getCurrentPosition(initMap);
