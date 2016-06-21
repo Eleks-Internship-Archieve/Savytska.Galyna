ac042da72d74fbe1bb88f1253d3034328f0bc95b
Savytska <otakadivchynka@gmail.com>
2016-06-21 11:35:59 +0300

    Initialize autocomplete

--- location.js
+++ location.js
@@ -1,5 +1,15 @@
 var map;
 var service;
+var placeSearch, autocomplete;
+var componentForm = {
+  street_number: 'short_name',
+  route: 'long_name',
+  locality: 'long_name',
+  administrative_area_level_1: 'short_name',
+  country: 'long_name',
+  postal_code: 'short_name'
+};
+
 function initMap(location) {
     
     console.log(location);  
@@ -13,69 +23,74 @@ function initMap(location) {
     
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     
-    var input = /** @type {!HTMLInputElement} */(
-      document.getElementById('pac-input'));
-
-  var types = document.getElementById('type-selector');
+   var input = (document.getElementById('autocomplete'));
+    var types = document.getElementById('type-selector');
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+    
+    autocomplete = new google.maps.places.Autocomplete(
+        (input),
+        {types: ['geocode']});
+        autocomplete.addListener('place_changed', fillInAddress);
+   
+    
+    
 
-  var autocomplete = new google.maps.places.Autocomplete(input);
-  autocomplete.bindTo('bounds', map);
   
-    var infowindow = new google.maps.InfoWindow();
-     var markerLoc = new google.maps.Marker({
-    map: map,
-    position: currentLocation
+  
+    var infowindow = new google.maps.InfoWindow(); 
+    var markerLoc = new google.maps.Marker({
+        map: map,
+        position: currentLocation
   });
     
     var marker = new google.maps.Marker({
-    map: map,
-    anchorPoint: new google.maps.Point(0, -29)
+    map: map    
   });
     
-    autocomplete.addListener('place_changed', function() {
-    infowindow.close();
-    marker.setVisible(false);
-    var place = autocomplete.getPlace();
+    
+    
+
+
+function fillInAddress() {
+  // Get the place details from the autocomplete object.
+  var place = autocomplete.getPlace();
     if (!place.geometry) {
       window.alert("Autocomplete's returned place contains no geometry");
       return;
     }
-    
-    // If the place has a geometry, then present it on a map.
     if (place.geometry.viewport) {
       map.fitBounds(place.geometry.viewport);
     } else {
       map.setCenter(place.geometry.location);
       map.setZoom(17); 
     }
-    marker.setIcon(/** @type {google.maps.Icon} */({
-      url: place.icon,
-      size: new google.maps.Size(71, 71),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(17, 34),
-      scaledSize: new google.maps.Size(35, 35)
-    }));
+ 
     marker.setPosition(place.geometry.location);
     marker.setVisible(true);
+    
+  for (var component in componentForm) {
+    document.getElementById(component).value = '';
+    document.getElementById(component).disabled = false;
+  }
 
-    var address = '';
-    if (place.address_components) {
-      address = [
-        (place.address_components[0] && place.address_components[0].short_name || ''),
-        (place.address_components[1] && place.address_components[1].short_name || ''),
-        (place.address_components[2] && place.address_components[2].short_name || '')
-      ].join(' ');
+  // Get each component of the address from the place details
+  // and fill the corresponding field on the form.
+  for (var i = 0; i < place.address_components.length; i++) {
+    var addressType = place.address_components[i].types[0];
+    if (componentForm[addressType]) {
+      var val = place.address_components[i][componentForm[addressType]];
+      document.getElementById(addressType).value = val;
     }
-
-    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-    infowindow.open(map, marker);
-  });
-
-  // Sets a listener on a radio button to change the filter type on Places
-  // Autocomplete.
-  function setupClickListener(id, types) {
+  }
+} 
+    
+    
+    
+ 
+    
+    
+    function setupClickListener(id, types) {
     var radioButton = document.getElementById(id);
     radioButton.addEventListener('click', function() {
       autocomplete.setTypes(types);
@@ -86,29 +101,10 @@ function initMap(location) {
   setupClickListener('changetype-address', ['address']);
   setupClickListener('changetype-establishment', ['establishment']);
   setupClickListener('changetype-geocode', ['geocode']);
-  
-   
-    
-    /*service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-        location: currentLocation,
-        radius: 10000, 
-        types: ['school']
-    }, callback);*/
-
-}
-/*
-function callback(results, status) {    
-    console.log(results);
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var markerS = new  google.maps.Marker({
-    map: map,
-    position: results[i].geometry.location
-  });
-    }
   }
-} */
 
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  
 
 navigator.geolocation.getCurrentPosition(initMap);
